a377722e97b84e991d6477ffb67f25c75e1c46af
David Janes <davidjanes@davidjanes.com>
2015-02-16 10:29:59 -0500

    configured -> configure

--- LIFXBridge.js
+++ LIFXBridge.js
@@ -253,12 +253,11 @@ LIFXBridge.prototype.reachable = function() {
 
 /**
  *  INSTANCE.
- *  Return True if this is configured. Things
- *  that are not configured are always not reachable.
- *  If not defined, "true" is returned
+ *  Configure an express web page to configure this Bridge.
+ *  Return the name of the Bridge, which may be 
+ *  listed and displayed to the user.
  */
-LIFXBridge.prototype.configured = function() {
-    return true;
+LIFXBridge.prototype.configure = function(app) {
 };
 
 /* --- injected: THIS CODE WILL BE REMOVED AT RUNTIME, DO NOT MODIFY  --- */
