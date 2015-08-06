34661da1afbbf9f55c75fade1ea2254b08530b8b
David Janes <davidjanes@davidjanes.com>
2015-08-06 10:54:02 +1000

    device -> device-id; thing -> thing-id

--- LIFXBridge.js
+++ LIFXBridge.js
@@ -222,8 +222,8 @@ LIFXBridge.prototype.meta = function () {
     }
 
     return {
-        "iot:thing": _.id.thing_urn.unique("LIFX", self.native.uuid, self.initd.number),
-        "iot:device": _.id.thing_urn.unique("LIFX", self.native.uuid),
+        "iot:thing-id": _.id.thing_urn.unique("LIFX", self.native.uuid, self.initd.number),
+        "iot:device-id": _.id.thing_urn.unique("LIFX", self.native.uuid),
         "schema:name": self.native.name || "LIFX",
         "schema:manufacturer": "schema:manufacturer",
     };
