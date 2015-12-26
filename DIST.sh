16257c01db20afa6ba5783868ffc8975f8955278
David Janes <davidjanes@davidjanes.com>
2015-12-26 12:38:48 -0330

    auto commit/push package.json

--- DIST.sh
+++ DIST.sh
@@ -36,12 +36,9 @@ echo "=================="
         LIFXBridge.js index.js products.json \
         models/*.js models/*.json \
         |
-    ( cd "${NPM_DST}" && tar xvf - )
-
-    ## cp dist/*.* "${NPM_DST}" || exit 1
-
-    cd "${NPM_DST}" || exit 1
-    npm publish
+    ( cd "${NPM_DST}" && tar xvf - && npm publish ) || exit 1
+    git commit -m "new release" package.json || exit 1
+    git push || exit 1
 
     echo "end"
 )
